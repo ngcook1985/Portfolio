e9dcbb6883f1010d02f2b7b92d60ae08af78f95a
ngcook1985 <ngcook1985@gmail.com>
2022-07-29 20:35:05 +1000

    Captains log, Day 0001. After hours lost at sea we have a functional (somewhat) random RPS generator. Started with player input

--- TOP-rock-paper-scissors/script.js
+++ TOP-rock-paper-scissors/script.js
@@ -1,10 +1,23 @@
-
+//Create R,P,S randomiser
+var compChoice
+var playerChoice
 //Create R,P,S randomiser
 function getComputerChoice(){
-    return Math.floor(Math.random() * 3)
-    
+   compChoice = Math.floor(Math.random() *3) 
+   
+   if (compChoice == 0){
+    compChoice = "Rock"
+   } 
+   else if (compChoice == 1){
+    compChoice = "Paper"
+   }
+   else {
+    compChoice = "Scissors"
+   }
 }
 
+//Input for user
+function getPlayerChoice(){
+    playerChoice = prompt("Rock, Paper or Scissors?")
+}
 
-let a = getComputerChoice()
-console.log(a)
